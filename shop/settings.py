dbb1a67571b10c657d2779806e0f1008bd54b59b
Kamil Stochmal <kamil.stochmal2@gmail.com>
2023-03-08 10:21:31 +0100

    Usunięcie maila

--- shop/settings.py
+++ shop/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'kamil.stochmal3@gmail.com'
-# EMAIL_HOST_PASSWORD = 'wrrvghdsgndgqjin' 
+# EMAIL_HOST_USER = '---'
+# EMAIL_HOST_PASSWORD = '---' 
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # DEFAULT_FROM_EMAIL = 'default from email'
